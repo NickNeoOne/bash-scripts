eb44ae94f756950589f1885e2c68103299f03f2a
NickNeo <33730384+NickNeoOne@users.noreply.github.com>
2021-04-22 11:48:08 +0400

    Create backup-config-and-bd.sh

---
+++ backup-config-and-bd.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+##########################################
+#### ПЕРЕМЕННЫЕ ПОЛЬЗОВАТЕЛЯ
+##########################################
+
+# Данные для подключения к БД
+HOSTNAME=asr-squid01
+USERNAME=username
+DB_NAME=DB_name
+export PGPASSWORD="PASSWORD"
+
+# Каталог куда сохраняется бекап, в нем будет создан подкаталог в соответствии с датой бекапа
+TARGET_DIR="/opt/backup/bd/`date '+%F'`/"
+# определяем Имя файла
+
+OUTPUT_FILE=${DB_NAME}-backup.psql.gz
+
+
+# каталоги которые будем бекапить можно указать несколько каталогов как указано ниже каждыйкаталог с новой строки.
+#например:
+#TGTD2="/mnt/smb/
+#/mnt/smb1"
+SOURCE_PATCH="/etc/squid
+/etc/default
+/var/www/html
+/usr/lib/squid
+/etc/krb5.conf"
+
+
+# Лог файл
+LOGFILE="$TARGET_DIR"bd_backup.log
+
+# Кому отправлять уведомление
+MAIL_TO=user-to@email.ru
+
+# Кому отправлять копию уведомления
+MAIL_TOCC=user2-to@email2.ru
+
+# От кого будем отправлять почту
+MAIL_FROM=bd-backup@email.ru
+
+# Указываем адрес и порт почтового сервера
+#MAIL_SERVER=10.10.10.42
+MAIL_SERVER=hostname.mail.server.ru
+
+MAIL_PORT=25
+
+echo "$HOSTNAME"
+echo "$USERNAME"
+echo "$DB_NAME"
+echo "$TARGET_DIR""$OUTPUT_FILE"
+
+mkdir -p "$TARGET_DIR"
+
+# Запускаем backup Базы данных
+pg_dump -C -h "$HOSTNAME" -U "$USERNAME" "$DB_NAME" | gzip > "$TARGET_DIR""$OUTPUT_FILE"
+#pg_dump -C -h "$HOSTNAME" -U "$USERNAME"  "$DB_NAME"   > "$TARGET_DIR""$OUTPUT_FILE".psql
+
+# Проверяем статус бекапа БД
+if [ -s "$TARGET_DIR""$OUTPUT_FILE" ]
+then
+        echo "+-------------------------------------------------------------------------------------+" >>$LOGFILE
+        echo "Файл бэкапа сохранен как \"$TARGET_DIR$OUTPUT_FILE\" с размером `ls -lh  "$TARGET_DIR""$OUTPUT_FILE" | awk '{print ($5)}'`byte" >>$LOGFILE
+        echo "Бэкап БД успешно выполнен в $(date +'%R %d-%b-%Y')" >>$LOGFILE
+        echo "+-------------------------------------------------------------------------------------+" >>$LOGFILE
+else
+        echo "+------------------------------------------------------------------+" >>$LOGFILE
+        echo "|  Сегодня $(date +'%R %d-%b-%Y') Произошла ошибка! Бэкап БД не удался.    |" >>$LOGFILE
+        echo "+------------------------------------------------------------------+" >>$LOGFILE
+fi
+
+# Очищаем переменную PGPASSWORD для безопасности
+export PGPASSWORD=""
+
+# Запускаем backup конфигурационных файлов
+OUTPUT_DATAFILE=config-backup.tar.gz
+tar pczvf  $TARGET_DIR$OUTPUT_DATAFILE  $SOURCE_PATCH
+
+# Проверяем статус бекапа
+STATUS=$?
+if [[ $STATUS != 0 ]]; then
+        echo "+------------------------------------------------------------------+" >>$LOGFILE
+        echo "|  Сегодня $(date +'%R %d-%b-%Y') Произошла ошибка! Бэкап конфигурационных файлов не удался.    |" >>$LOGFILE
+        echo "+------------------------------------------------------------------+" >>$LOGFILE
+else
+        echo "+-------------------------------------------------------------------------------------+" >>$LOGFILE
+        echo "Файл бэкапа сохранен как \"$TARGET_DIR$OUTPUT_FILE\" с размером `ls -lh  "$TARGET_DIR""$OUTPUT_DATAFILE" | awk '{print ($5)}'`byte" >>$LOGFILE
+        echo "Бэкап конфигурационных файлов  успешно выполнен в $(date +'%R %d-%b-%Y')" >>$LOGFILE
+        echo "+-------------------------------------------------------------------------------------+" >>$LOGFILE
+fi
+
+# Если необходимо шлем уведомбение в почту
+# sendEmail -f "$MAIL_FROM"  -t "$MAIL_TO" -cc "$MAIL_TOCC"  -u backup "$BACKUP_STATUS" -s "$MAIL_SERVER":"$MAIL_PORT"  -o message-charset=UTF-8 -o message-file="$LOGFILE"
+exit
